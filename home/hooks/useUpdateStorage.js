002c5f26d4236a7bc1351e33db05d76e8fe895b0
cristian-rios <cristian.rios@moxit.net>
2020-09-04 14:52:54 -0300

    Eliminada carpeta de home innecesaria del repo que no hacia falta que este

--- home/hooks/useUpdateStorage.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { useSelector } from 'react-redux';
-
-const useUpdateStorage = () => {
-    const update = useSelector(state => state.settings.update)
-    return update;
-}
-
-export default useUpdateStorage;
