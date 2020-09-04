a156e976f049dce797bd6093984ddae3620f26df
cristian-rios <cristian.rios@moxit.net>
2020-09-04 16:21:30 -0300

    zpl en debug

--- components/preview/zpl.js
+++ components/preview/zpl.js
@@ -1,16 +1,41 @@
 export const generateQR = (pallet, lot) => {
-    const {matCode, lotNo, qty} = getLotFields(lot);
-    const gs1 = `00${pallet}20${matCode}10${lotNo}37${qty}`;
-    return `@"CT~~CD,~CC^~CT~
-        ^XA~TA000~JSN^LT0^MNW^MTD^PON^PMN^LH0,0^JMA^PR4,4~SD15^JUS^LRN^CI0^XZ
-        ^XA
-        ^MMT
-        ^PW799
-        ^LL0480
-        ^LS0
-        ^BY3,3,111^FT100,200^BQN,0,6Y,N
-        ^FDM,${gs1}^FS
-        ^PQ1,0,1,Y^XZ"`;
+
+    // pallet = 1234
+    // lot = 12345678ABCDEF1234
+
+    // const {matCode, lotNo, qty} = getLotFields(lot);
+    const pallet = 50000000001;
+    const matCode = 80108274;
+    const lot = 'ARACJ4';
+    const qty = 6720;
+
+    // const zpl = 
+    // `^XA~TA000~JSN^LT0^MNW^MTT^PON^PMN^LH0,0^JMA^PR6,6~SD15^JUS^LRN^CI0^XZ
+    // ^XA
+    // ^MMT
+    // ^PW799
+    // ^LL0519
+    // ^LS0
+    
+    // ^BY198,198^FT435,385^BXN,9,200,0,0,1,~
+    // ^FH\\^FD\\7E192${pallet}\\7E1240${matCode}\\7E110${lot}\\7E137${qty}^FS
+    // ^PQ1,0,1,Y^XZ
+    // `;
+
+    const zpl = 
+    `^XA~TA000~JSN^LT0^MNW^MTT^PON^PMN^LH0,0^JMA^PR6,6~SD15^JUS^LRN^CI0^XZ
+    ^XA
+    ^MMT
+    ^PW799
+    ^LL0519
+    ^LS0
+    
+    ^BY198,198^FT435,385^BXN,9,200,0,0,1,~
+    ^FH\\^FD\\7E19250000000001\\7E124080108274\\7E110ARACJ4\\7E1376720^FS
+    ^PQ1,0,1,Y^XZ
+    `;
+
+    return zpl;
 };
 
 export const getLotFields = (lot) => (!lot || lot.length < 18) ? {
