e7acc272a29ade8f23e345118c202a26f6bf68a3
cristian-rios <cristian.rios@moxit.net>
2020-09-25 14:43:45 -0300

    Size correcto en popup

--- helper/components.js
+++ helper/components.js
@@ -4,13 +4,14 @@ import { Button, Icon, Overlay, Text } from 'react-native-elements';
 
 export const Popup = ({visible, title, text, width, type, onPress}) => {
     return (
-        <Overlay isVisible={visible} animationType={"fade"} overlayStyle={{height: '50%', width: width}}>
-            <View style={{height: '100%', flexDirection: "column", justifyContent: "space-evenly", padding: 10}}>
+        <Overlay isVisible={visible} animationType={"fade"} overlayStyle={{width: width}}>
+            <View style={{flexDirection: "column", justifyContent: "space-evenly", padding: 10}}>
                 <Icon name={type === 'approved' ? 'check-circle' : type === 'rejected' ? 'x-circle' : 'alert-circle'}
                     type='feather' 
                     color={type === 'approved' ? '#00C18A' : type === 'rejected' ? '#DB3834' : '#38A6E3'}
-                    size={100}/>
-                <View style={{alignContent: 'center', flexDirection: "column", justifyContent: "center"}}>
+                    size={80}
+                    />
+                <View style={{alignContent: 'center', flexDirection: "column", justifyContent: "center", padding: 20}}>
                     {title && 
                         <View style={{alignContent: 'center', flexDirection: "row", justifyContent: "center"}}>
                             <Text style={{fontSize: 21, fontWeight: 'bold', color: '#9C9C9C'}}>{title}</Text>
